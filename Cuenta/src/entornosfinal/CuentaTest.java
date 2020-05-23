b3dc40e65023e7adbc115be3fb89e3dca09d92f2
jorge.lopezbermejo2@educa.madrid.org <jorge@DESKTOP-9PS0MQQ>
2020-05-23 22:17:01 +0200

    test

---
+++ Cuenta/src/entornosfinal/CuentaTest.java
@@ -0,0 +1,21 @@
+package entornosfinal;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class CuentaTest {
+	@Test
+	public void ingreso(){
+		Cuenta cuenta1 = new Cuenta("Jorge", "lopez", "Móstoles", 50365525, 680906267, 21, 13456789, 2580.95 );
+		assertTrue(cuenta1.ingreso(100));
+	}
+	@Test
+	public void retirada(){
+		Cuenta cuenta1 = new Cuenta("Jorge", "lopez", "Móstoles", 50365525, 680906267, 21, 13456789, 2580.95 );
+		assertFalse(cuenta1.retirada(-1));
+	}
+	 }
